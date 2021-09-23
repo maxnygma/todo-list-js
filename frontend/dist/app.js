019b1ce549fcd6a2e3ef573b4419dc8dd0b2ce36
xzcodes <dwdcodes@gmail.com>
2021-09-23 20:44:54 +0700

    First commit

---
+++ frontend/dist/app.js
@@ -0,0 +1,58 @@
+var inputBox = document.querySelector(".task-input input");
+var addButton = document.querySelector(".task-input button");
+var todoList = document.querySelector(".todo-list");
+var deleteAllButton = document.querySelector(".clear-tasks");
+inputBox.onkeyup = function () {
+    var userData = inputBox.value;
+    if (userData.trim().length != 0) {
+        addButton.classList.add("active");
+    }
+    else {
+        addButton.classList.remove("active");
+    }
+};
+addButton.onclick = function () {
+    var userData = inputBox.value;
+    var getLocalStorage = localStorage.getItem("New Todo");
+    var listArr;
+    if (getLocalStorage == null) {
+        listArr = [];
+    }
+    else {
+        listArr = JSON.parse(getLocalStorage);
+    }
+    listArr.push(userData);
+    localStorage.setItem("New Todo", JSON.stringify(listArr));
+    showTasks();
+};
+var showTasks = function () {
+    var getLocalStorage = localStorage.getItem("New Todo");
+    var listArr;
+    if (getLocalStorage == null) {
+        listArr = [];
+    }
+    else {
+        listArr = JSON.parse(getLocalStorage);
+    }
+    var newTag = '';
+    listArr.forEach(function (element, index) {
+        newTag += "<li> " + element + " <span onclick=\"deleteTask(" + index + ")\" class=\"material-icons delete-icon\">delete</span></li>";
+    });
+    todoList.innerHTML = newTag;
+    inputBox.value = "";
+};
+var deleteTask = function (index) {
+    var getLocalStorage = localStorage.getItem("New Todo");
+    var listArr = JSON.parse(getLocalStorage);
+    listArr.splice(index, 1);
+    localStorage.setItem("New Todo", JSON.stringify(listArr));
+    showTasks();
+};
+deleteAllButton.onclick = function () {
+    var getLocalStorage = localStorage.getItem("New Todo");
+    var listArr = JSON.parse(getLocalStorage);
+    listArr = [];
+    localStorage.setItem("New Todo", JSON.stringify(listArr));
+    showTasks();
+};
+showTasks();
---
+++ frontend/src/app.ts
@@ -0,0 +1,77 @@
+const inputBox = document.querySelector(".task-input input") as HTMLInputElement
+const addButton = document.querySelector(".task-input button") as HTMLButtonElement
+const todoList = document.querySelector(".todo-list") as HTMLUListElement
+const deleteAllButton = document.querySelector(".clear-tasks") as HTMLButtonElement
+
+inputBox.onkeyup = () => {
+    let userData = inputBox.value
+    if (userData.trim().length != 0) {
+        addButton.classList.add("active")
+    }
+    else {
+        addButton.classList.remove("active")
+    }
+}
+
+addButton.onclick = () => {
+    let userData = inputBox.value
+    let getLocalStorage = localStorage.getItem("New Todo")
+    let listArr: string[]
+
+    if (getLocalStorage == null) {
+        listArr = []
+    }
+    else {
+        listArr = JSON.parse(getLocalStorage)
+    }
+
+    listArr.push(userData)
+    localStorage.setItem("New Todo", JSON.stringify(listArr))
+
+    showTasks()
+}
+
+let showTasks = () => {
+    let getLocalStorage = localStorage.getItem("New Todo")
+    let listArr: string[]
+
+    if (getLocalStorage == null) {
+        listArr = []
+    }
+    else {
+        listArr = JSON.parse(getLocalStorage)
+    }
+
+    let newTag = ''
+    listArr.forEach((element: string, index: number) => {
+        newTag += `<li> ${element} <span onclick="deleteTask(${index})" class="material-icons delete-icon">delete</span></li>`
+    })
+
+    todoList.innerHTML = newTag
+    inputBox.value = ""
+}
+
+let deleteTask = (index: number) => {
+    let getLocalStorage = localStorage.getItem("New Todo")
+    let listArr = JSON.parse(getLocalStorage)
+    listArr.splice(index, 1)
+
+    localStorage.setItem("New Todo", JSON.stringify(listArr))
+
+    showTasks()
+}
+
+deleteAllButton.onclick = () => {
+    let getLocalStorage = localStorage.getItem("New Todo")
+    let listArr = JSON.parse(getLocalStorage)
+
+    listArr = []
+
+    localStorage.setItem("New Todo", JSON.stringify(listArr))
+
+    showTasks()
+}
+
+showTasks()
+
+
